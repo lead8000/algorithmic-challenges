b5e34c57b30b222b8843fc2ca0e63a427ac2074a
leandro-driguez <leandro__rodriguez@outlook.com>
2022-11-10 04:38:58 -0500

    feat: add the solutions of the day

---
+++ GeeksForGeeks/getMiddle.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+// } Driver Code Ends
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+class Solution{
+    public:
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    int getMiddle(Node *head)
+    {
+        int amountNodes = 0;
+        for (auto i = head; i != nullptr; i=i->next, amountNodes++) { }
+        
+        int middle = (int)(amountNodes/2) + 1;
+        for (auto i = head; i != nullptr; i=i->next, middle--) 
+        {
+            if (middle == 1)
+                return i->data;
+        }
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        cout << ob.getMiddle(head) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
